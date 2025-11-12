9d5982d25fc0cbcec17390c78a97f7055cc00a4f
AndrewX <andrewx@bk.ru>
2025-11-12 11:33:50 +0300

    Done. Practice with parametr many=

--- QuoteApi/ma_examples/deserialize.py
+++ QuoteApi/ma_examples/deserialize.py
@@ -1,6 +1,7 @@
 from author import Author
 from schema import AuthorSchema
 from marshmallow import ValidationError, RAISE, INCLUDE, EXCLUDE
+from pprint import pprint
 
 
 json_data = """
@@ -21,6 +22,39 @@ except ValidationError as e:
     print(e.messages_dict)
 
 
-result = schema.load(json_data_as_string, unknown=INCLUDE)
+result = schema.load(json_data_as_string, unknown=EXCLUDE)
 print(type(result), result)
-print(Author(**result))
+#print(Author(**result))
+
+
+json_data_list = """
+[
+{
+"id": 1,
+"name": "Alex",
+"email": "alex@mail.ru"
+},
+{
+"id": 2,
+"name": "Ivan",
+"email": "ivan@mail.ru"
+},
+{
+"id": 4,
+"name": "Tom",
+"email": "tom@mail.ru"
+}
+]
+"""
+
+# При обработке списка необходимо указать параметр many=True
+# Лиюо при создании экземпляра схемы при вызове методов load, loads
+# Exmple 1
+authors_schema = AuthorSchema(many=True)
+result_one = authors_schema.loads(json_data_list)
+pprint(result_one, sort_dicts=False)
+
+#Example 2
+result_two = schema.loads(json_data_list, many=True)
+print('\n===========Repeat===========\n')
+pprint(result_two, sort_dicts=False)
--- QuoteApi/ma_examples/schema.py
+++ QuoteApi/ma_examples/schema.py
@@ -2,7 +2,7 @@ from marshmallow import Schema, fields
 
 
 class AuthorSchema(Schema):
-    id = fields.Integer(strict=True)
+    id = fields.Integer()
     name = fields.String(required=True, error_messages={"required": "field 'name' is required"})
     email = fields.Email(required=True, error_messages={"required": "field 'email' is required"})
     surname = fields.String()
