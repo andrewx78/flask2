464638e9b8194618aeddaf6ca696032859979f4b
AndrewX <andrewx@bk.ru>
2025-11-12 11:03:46 +0300

    Add MA load examples

--- QuoteApi/ma_examples/deserialize.py
+++ QuoteApi/ma_examples/deserialize.py
@@ -1,25 +1,26 @@
 from author import Author
 from schema import AuthorSchema
-from marshmallow import ValidationError
+from marshmallow import ValidationError, RAISE, INCLUDE, EXCLUDE
 
 
 json_data = """
 {
     "id": 12,
     "name": "Ivan Petrov",
-    "email": "ivan@mail.ru"
+    "email": "ivan@mail.ru",
+    "age": 34
 }
 """
 try:
     schema = AuthorSchema()
     # json string -> validated dict
-    json_data_as_string = schema.loads(json_data)
+    json_data_as_string = schema.loads(json_data, unknown=EXCLUDE)
     print(type(json_data_as_string), json_data_as_string)
 except ValidationError as e:
     print(e)
     print(e.messages_dict)
 
 
-result = schema.load(json_data_as_string)
+result = schema.load(json_data_as_string, unknown=INCLUDE)
 print(type(result), result)
 print(Author(**result))
--- QuoteApi/ma_examples/schema.py
+++ QuoteApi/ma_examples/schema.py
@@ -3,6 +3,6 @@ from marshmallow import Schema, fields
 
 class AuthorSchema(Schema):
     id = fields.Integer(strict=True)
-    name = fields.String(required=True, error_messages={"required": "field 'name' id required"})
-    email = fields.Email()
+    name = fields.String(required=True, error_messages={"required": "field 'name' is required"})
+    email = fields.Email(required=True, error_messages={"required": "field 'email' is required"})
     surname = fields.String()
