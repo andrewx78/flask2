b074f0aaf619686d2f4e60264b521ab4ac9e4aa3
AndrewX <andrewx@bk.ru>
2025-11-13 11:35:07 +0300

    Add save and delete methods to UserModel

--- QuoteApi/api/models/user.py
+++ QuoteApi/api/models/user.py
@@ -1,9 +1,14 @@
+from flask import abort
 from passlib.apps import custom_app_context as pwd_context
 import sqlalchemy.orm as so
 from api import db
 import sqlalchemy as sa
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 
 class UserModel(db.Model):
+    __tablename__ = "users"
+
+
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(32),index=True, unique=True)
     password_hash: so.Mapped[str] = so.mapped_column(sa.String(128))
@@ -19,3 +24,22 @@ class UserModel(db.Model):
     def verify_password(self, password):
         return pwd_context.verify(password, self.password_hash)
     
+    def save(self):
+        try:
+            db.session.add(self)
+            db.session.commit()
+        except InterruptedError as e:
+            db.session.rollback()
+            abort(400, f"Database integrity error: {str(e)}")
+        except SQLAlchemyError as e:
+            db.session.rollback()
+            abort(503, f"Database error: {str(e)}")
+
+
+    def delete(self):
+        try:
+            db.session.delete(self)
+            db.session.commit()
+        except SQLAlchemyError as e:
+            db.session.rollback()
+            abort(503, f"Database error: {str(e)}")
